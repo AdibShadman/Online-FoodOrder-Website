2d4d39e6d99b85c15f8c5ee588999f5b73f3e2a7
Adib-shadman <40189935+Adib-shadman@users.noreply.github.com>
2020-01-15 20:29:07 +1100

    Update db_conn.php

--- Assignment2_468684/db_conn.php
+++ Assignment2_468684/db_conn.php
@@ -1,6 +1,6 @@
 <?php
 //mysql connection (hostname, username, password, dbname);
-$mysqli = new mysqli('localhost', 'asornob', '468684', 'asornob');
+$mysqli = new mysqli('localhost', 'root', '', 'asornob');
 
 //check connection
 if (mysqli_connect_errno()) {
